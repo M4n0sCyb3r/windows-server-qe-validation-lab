866daa68606c6791a0d1c5bd5a17b46a66a4ea5c
M4n0sCyb3r <cybertechjmg@gmail.com>
2026-08-28 23:53:50 -0700

    Add recent System event health validation (#15)

--- scripts/run-ci-checks.sh
+++ scripts/run-ci-checks.sh
@@ -71,6 +71,12 @@ ansible-playbook \
   --syntax-check \
   --ask-vault-password
 
+ansible-playbook \
+  -i inventory/hosts.yml \
+  playbooks/srv01-event-health-negative-validation-suite.yml \
+  --syntax-check \
+  --ask-vault-password
+
 echo "PASS: Ansible syntax"
 echo
 
