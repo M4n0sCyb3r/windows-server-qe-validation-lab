b01cfc9463bd30d2387f949b5a091d6721541ec2
M4n0sCyb3r <cybertechjmg@gmail.com>
2026-09-02 20:51:13 -0700

    Add Advanced Audit Policy validation (#22)

--- scripts/run-ci-checks.sh
+++ scripts/run-ci-checks.sh
@@ -113,6 +113,12 @@ ansible-playbook \
   --syntax-check \
   --ask-vault-password
 
+ansible-playbook \
+  -i inventory/hosts.yml \
+  playbooks/srv01-audit-policy-negative-validation-suite.yml \
+  --syntax-check \
+  --ask-vault-password
+
 echo "PASS: Ansible syntax"
 echo
 
